04b93f5e1f93158fbf35718e55cc20ea43226737
nasty091 <6151071035@st.utc2.edu.vn>
2024-03-16 13:44:36 +0700

    Web Project DTO's and API Models

---
+++ MagicVilla_Utility/SD.cs
@@ -0,0 +1,13 @@
+﻿namespace MagicVilla_Utility
+{
+    public static class SD //Static Data
+    {
+        public enum ApiType
+        {
+            GET,
+            POST, 
+            PUT, 
+            DELETE
+        }
+    }
+}
